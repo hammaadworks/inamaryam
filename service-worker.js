b7e1e47c6415efd9f6ded286626b4cd20f5150b6
hammaad-swe <hammaad.swe@gmail.com>
2025-06-14 09:06:00 +0530

    feat: pwa v1.1

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,11 @@ self.addEventListener("install", function (event) {
 self.addEventListener("activate", function (event) {
   console.log("Service worker activated.");
 });
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
